0fdc519136bca0cabe0b8ce08851cfac616f2836
James Zhang <jchunzh@gmail.com>
2014-09-16 13:05:53 -0400

    Initial commit

---
+++ Demo/Demo/MessageFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace Demo
+{
+    public class MessageFactory : IMessageFactory
+    {
+        public string GetGreeting(string name)
+        {
+            return string.Format("Hello {0}", name);
+        }
+
+        public string GetGoodbye(string name)
+        {
+            return string.Format("Goodbye {0}", name);
+        }
+    }
+}
---
+++ Demo/Demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMessageFactory messageFactory = new MessageFactory();
+            
+            Console.WriteLine(messageFactory.GetGreeting("James"));
+            Console.WriteLine(messageFactory.GetGoodbye("James"));
+
+            Console.ReadLine();
+        }
+    }
+}
